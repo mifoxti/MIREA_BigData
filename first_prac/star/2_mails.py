6ce0157718536cbc79ae58b0be2ea024577b2d4c
mifoxti <52703409+mifoxti@users.noreply.github.com>
2025-09-18 17:25:56 +0300

    Маленький фикс второй задачи, добавил отчет.

--- first_prac/star/2_mails.py
+++ first_prac/star/2_mails.py
@@ -1,16 +1,21 @@
-names = []
+names = {}
 
 
 def main(name):
-    if name not in names:
-        names.append(name)
+    if name not in names.keys():
+        names[name] = 1
         return 'OK'
     else:
         return gen_name(name)
 
 
 def gen_name(old_name):
-    new_name = f"{old_name}{names.count(old_name)}"
+    new_name = f"{old_name}{names[old_name]}"
+    names[old_name] += 1
+    if new_name in names.keys():
+        names[new_name] += 1
+    else:
+        names[new_name] = 1
     return new_name
 
 
